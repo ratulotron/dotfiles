c060d424de8624a6dc8d7a0c0be6d3185c7da531
Ratul Minhaz <minhaz.ratul@gmail.com>
2026-02-08 23:25:14 +0100

    broot: add launcher sourcing and stow ignore

--- broot/init.zsh
+++ broot/init.zsh
@@ -17,3 +17,8 @@ if command -v broot >/dev/null 2>&1; then
         fi
     }
 fi
+
+# Broot launcher
+if [ -f "$HOME/.config/broot/launcher/bash/br" ]; then
+  source "$HOME/.config/broot/launcher/bash/br"
+fi
