1adb8e41ad1d59c8b15acd3e066e56e7755c59e2
Ratul Minhaz <minhaz.ratul@gmail.com>
2026-05-12 11:24:31 +0200

    zsh: disable autocorrect for go command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Disable zsh autocorrect for commands whose arguments look like paths but aren't
+alias go='nocorrect go'
